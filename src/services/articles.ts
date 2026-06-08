49abb531012e479f09e8252462eb4cdbf505dcbc
lmerkel <lmerkel@unistra.fr>
2026-06-08 15:44:01 +0200

    starting api implementations, create route

---
+++ src/services/articles.ts
@@ -0,0 +1,31 @@
+import axios, { type AxiosInstance } from 'axios'
+
+type Articles = {
+  title: string;
+  description: string;
+  slug: string;
+  createdAt: string;
+  publishedAt: string;
+};
+
+const api: AxiosInstance = axios.create({
+  baseURL: import.meta.env.BASE_URL_API,
+  timeout: 5000,
+  headers: { Authorization: `Bearer ${import.meta.env.TOKEN_API}` },
+})
+
+export async function getArticles(): Promise<Articles[] | null>{
+  try {
+    const response = await api.get<Articles[]>(`/articles`)
+    return response.data
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.message)
+    }
+    else {
+      console.error('Erreur inatendue:',error)
+    }
+    return null
+  }
+}
+
