ed0328e0ad4e2d4edd43e1973c6ec1002fb435b4
Léo <cdf.leo.merkel@gmail.com>
2026-08-20 23:54:40 +0200

    adjust config build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: 'https://lp-dwca.lmerkel.fr',
+  trailingSlash: 'always',
+  output: 'static',
+})
